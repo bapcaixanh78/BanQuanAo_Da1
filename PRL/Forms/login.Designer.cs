27704ddecb0257f44fa949854cd6a00f27750e4a
Manh5112003 <manhnvph43305@fpt.edu.vn>
2023-11-16 21:42:12 +0700

    ok

---
+++ PRL/Forms/login.Designer.cs
@@ -0,0 +1,272 @@
+﻿namespace PRL.Forms
+{
+    partial class login
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(login));
+            label6 = new Label();
+            button1 = new Button();
+            label5 = new Label();
+            panel3 = new Panel();
+            panel4 = new Panel();
+            pictureBox3 = new PictureBox();
+            textBox2 = new TextBox();
+            label4 = new Label();
+            label3 = new Label();
+            label2 = new Label();
+            textBox1 = new TextBox();
+            panel2 = new Panel();
+            panel5 = new Panel();
+            pictureBox2 = new PictureBox();
+            pictureBox1 = new PictureBox();
+            label1 = new Label();
+            panel1 = new Panel();
+            panel3.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)pictureBox3).BeginInit();
+            panel2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)pictureBox2).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
+            panel1.SuspendLayout();
+            SuspendLayout();
+            // 
+            // label6
+            // 
+            label6.AutoSize = true;
+            label6.Font = new Font("Century Gothic", 10.2F, FontStyle.Regular, GraphicsUnit.Point);
+            label6.Location = new Point(53, 614);
+            label6.Name = "label6";
+            label6.Size = new Size(314, 21);
+            label6.TabIndex = 9;
+            label6.Text = "Developed by: Programming Source ";
+            // 
+            // button1
+            // 
+            button1.BackColor = Color.FromArgb(155, 89, 182);
+            button1.FlatStyle = FlatStyle.Flat;
+            button1.Font = new Font("Century Gothic", 12F, FontStyle.Regular, GraphicsUnit.Point);
+            button1.ForeColor = SystemColors.ButtonHighlight;
+            button1.Location = new Point(43, 515);
+            button1.Name = "button1";
+            button1.Size = new Size(324, 35);
+            button1.TabIndex = 8;
+            button1.Text = "Login";
+            button1.UseVisualStyleBackColor = false;
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.Font = new Font("Century Gothic", 10.2F, FontStyle.Regular, GraphicsUnit.Point);
+            label5.Location = new Point(73, 147);
+            label5.Name = "label5";
+            label5.Size = new Size(294, 21);
+            label5.TabIndex = 7;
+            label5.Text = "Easy and fast prodoct Manament";
+            // 
+            // panel3
+            // 
+            panel3.Controls.Add(panel4);
+            panel3.Controls.Add(pictureBox3);
+            panel3.Controls.Add(textBox2);
+            panel3.Location = new Point(57, 438);
+            panel3.Name = "panel3";
+            panel3.Size = new Size(303, 35);
+            panel3.TabIndex = 6;
+            // 
+            // panel4
+            // 
+            panel4.BackColor = Color.FromArgb(155, 89, 182);
+            panel4.Location = new Point(3, 34);
+            panel4.Name = "panel4";
+            panel4.Size = new Size(304, 1);
+            panel4.TabIndex = 6;
+            // 
+            // pictureBox3
+            // 
+            pictureBox3.Image = (Image)resources.GetObject("pictureBox3.Image");
+            pictureBox3.Location = new Point(269, 3);
+            pictureBox3.Name = "pictureBox3";
+            pictureBox3.Size = new Size(25, 26);
+            pictureBox3.SizeMode = PictureBoxSizeMode.Zoom;
+            pictureBox3.TabIndex = 5;
+            pictureBox3.TabStop = false;
+            // 
+            // textBox2
+            // 
+            textBox2.BorderStyle = BorderStyle.None;
+            textBox2.Location = new Point(2, 10);
+            textBox2.Name = "textBox2";
+            textBox2.Size = new Size(255, 20);
+            textBox2.TabIndex = 0;
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Font = new Font("Century Gothic", 10.2F, FontStyle.Regular, GraphicsUnit.Point);
+            label4.Location = new Point(59, 414);
+            label4.Name = "label4";
+            label4.Size = new Size(88, 21);
+            label4.TabIndex = 5;
+            label4.Text = "Password";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Font = new Font("Century Gothic", 10.2F, FontStyle.Regular, GraphicsUnit.Point);
+            label3.Location = new Point(55, 325);
+            label3.Name = "label3";
+            label3.Size = new Size(93, 21);
+            label3.TabIndex = 3;
+            label3.Text = "Username";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Font = new Font("Century Gothic", 15F, FontStyle.Regular, GraphicsUnit.Point);
+            label2.Location = new Point(84, 236);
+            label2.Name = "label2";
+            label2.Size = new Size(217, 31);
+            label2.TabIndex = 2;
+            label2.Text = "Welcome back !";
+            // 
+            // textBox1
+            // 
+            textBox1.BorderStyle = BorderStyle.None;
+            textBox1.Location = new Point(4, 13);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(255, 20);
+            textBox1.TabIndex = 0;
+            textBox1.Text = "usernamehere";
+            // 
+            // panel2
+            // 
+            panel2.Controls.Add(panel5);
+            panel2.Controls.Add(pictureBox2);
+            panel2.Controls.Add(textBox1);
+            panel2.Location = new Point(55, 349);
+            panel2.Name = "panel2";
+            panel2.Size = new Size(303, 35);
+            panel2.TabIndex = 4;
+            // 
+            // panel5
+            // 
+            panel5.BackColor = Color.FromArgb(155, 89, 182);
+            panel5.Location = new Point(2, 34);
+            panel5.Name = "panel5";
+            panel5.Size = new Size(305, 1);
+            panel5.TabIndex = 10;
+            // 
+            // pictureBox2
+            // 
+            pictureBox2.Image = (Image)resources.GetObject("pictureBox2.Image");
+            pictureBox2.Location = new Point(271, 3);
+            pictureBox2.Name = "pictureBox2";
+            pictureBox2.Size = new Size(25, 26);
+            pictureBox2.SizeMode = PictureBoxSizeMode.Zoom;
+            pictureBox2.TabIndex = 5;
+            pictureBox2.TabStop = false;
+            // 
+            // pictureBox1
+            // 
+            pictureBox1.Image = (Image)resources.GetObject("pictureBox1.Image");
+            pictureBox1.Location = new Point(73, 71);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(68, 63);
+            pictureBox1.SizeMode = PictureBoxSizeMode.Zoom;
+            pictureBox1.TabIndex = 0;
+            pictureBox1.TabStop = false;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Century Gothic", 19.8000011F, FontStyle.Regular, GraphicsUnit.Point);
+            label1.Location = new Point(147, 81);
+            label1.Name = "label1";
+            label1.Size = new Size(0, 40);
+            label1.TabIndex = 3;
+            // 
+            // panel1
+            // 
+            panel1.BackColor = Color.White;
+            panel1.Controls.Add(label6);
+            panel1.Controls.Add(button1);
+            panel1.Controls.Add(label5);
+            panel1.Controls.Add(panel3);
+            panel1.Controls.Add(label4);
+            panel1.Controls.Add(panel2);
+            panel1.Controls.Add(label3);
+            panel1.Controls.Add(label2);
+            panel1.Controls.Add(pictureBox1);
+            panel1.Controls.Add(label1);
+            panel1.Location = new Point(285, 6);
+            panel1.Name = "panel1";
+            panel1.Size = new Size(431, 646);
+            panel1.TabIndex = 1;
+            // 
+            // login
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(1016, 674);
+            Controls.Add(panel1);
+            FormBorderStyle = FormBorderStyle.None;
+            Name = "login";
+            StartPosition = FormStartPosition.CenterScreen;
+            Text = "login";
+            panel3.ResumeLayout(false);
+            panel3.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)pictureBox3).EndInit();
+            panel2.ResumeLayout(false);
+            panel2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)pictureBox2).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
+            panel1.ResumeLayout(false);
+            panel1.PerformLayout();
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private Label label6;
+        private Button button1;
+        private Label label5;
+        private Panel panel3;
+        private Panel panel4;
+        private PictureBox pictureBox3;
+        private TextBox textBox2;
+        private Label label4;
+        private Label label3;
+        private Label label2;
+        private TextBox textBox1;
+        private Panel panel2;
+        private Panel panel5;
+        private PictureBox pictureBox2;
+        private PictureBox pictureBox1;
+        private Label label1;
+        private Panel panel1;
+    }
+}
