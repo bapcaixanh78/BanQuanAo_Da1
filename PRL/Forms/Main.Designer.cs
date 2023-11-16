bc308216c8b45128036ea027968b061b72a12fda
Manh5112003 <manhnvph43305@fpt.edu.vn>
2023-11-16 13:54:52 +0700

    sfh

--- PRL/Forms/Main.Designer.cs
+++ PRL/Forms/Main.Designer.cs
@@ -29,113 +29,210 @@
         private void InitializeComponent()
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Main));
-            label1 = new Label();
+            panel1 = new Panel();
+            panel3 = new Panel();
+            pictureBox3 = new PictureBox();
+            textBox2 = new TextBox();
+            label4 = new Label();
+            panel2 = new Panel();
+            pictureBox2 = new PictureBox();
+            textBox1 = new TextBox();
+            label3 = new Label();
+            label2 = new Label();
             pictureBox1 = new PictureBox();
-            grb_DN = new GroupBox();
-            ptr_DN = new PictureBox();
-            txt_TenDN = new TextBox();
+            label1 = new Label();
+            label5 = new Label();
             button1 = new Button();
-            txt_MK = new TextBox();
+            panel1.SuspendLayout();
+            panel3.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)pictureBox3).BeginInit();
+            panel2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)pictureBox2).BeginInit();
             ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
-            grb_DN.SuspendLayout();
-            ((System.ComponentModel.ISupportInitialize)ptr_DN).BeginInit();
             SuspendLayout();
             // 
-            // label1
+            // panel1
             // 
-            label1.AutoSize = true;
-            label1.Location = new Point(136, 28);
-            label1.Name = "label1";
-            label1.Size = new Size(0, 20);
-            label1.TabIndex = 3;
+            panel1.BackColor = Color.White;
+            panel1.Controls.Add(button1);
+            panel1.Controls.Add(label5);
+            panel1.Controls.Add(panel3);
+            panel1.Controls.Add(label4);
+            panel1.Controls.Add(panel2);
+            panel1.Controls.Add(label3);
+            panel1.Controls.Add(label2);
+            panel1.Controls.Add(pictureBox1);
+            panel1.Controls.Add(label1);
+            panel1.Location = new Point(268, 6);
+            panel1.Name = "panel1";
+            panel1.Size = new Size(431, 632);
+            panel1.TabIndex = 0;
+            // 
+            // panel3
+            // 
+            panel3.Controls.Add(pictureBox3);
+            panel3.Controls.Add(textBox2);
+            panel3.Location = new Point(45, 435);
+            panel3.Name = "panel3";
+            panel3.Size = new Size(303, 41);
+            panel3.TabIndex = 6;
+            // 
+            // pictureBox3
+            // 
+            pictureBox3.Image = (Image)resources.GetObject("pictureBox3.Image");
+            pictureBox3.Location = new Point(269, 10);
+            pictureBox3.Name = "pictureBox3";
+            pictureBox3.Size = new Size(25, 26);
+            pictureBox3.SizeMode = PictureBoxSizeMode.Zoom;
+            pictureBox3.TabIndex = 5;
+            pictureBox3.TabStop = false;
+            // 
+            // textBox2
+            // 
+            textBox2.BorderStyle = BorderStyle.None;
+            textBox2.Location = new Point(2, 10);
+            textBox2.Name = "textBox2";
+            textBox2.Size = new Size(255, 16);
+            textBox2.TabIndex = 0;
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Font = new Font("Century Gothic", 10.2F, FontStyle.Regular, GraphicsUnit.Point);
+            label4.Location = new Point(47, 411);
+            label4.Name = "label4";
+            label4.Size = new Size(88, 21);
+            label4.TabIndex = 5;
+            label4.Text = "Password";
+            // 
+            // panel2
+            // 
+            panel2.Controls.Add(pictureBox2);
+            panel2.Controls.Add(textBox1);
+            panel2.Location = new Point(43, 346);
+            panel2.Name = "panel2";
+            panel2.Size = new Size(303, 36);
+            panel2.TabIndex = 4;
+            // 
+            // pictureBox2
+            // 
+            pictureBox2.Image = (Image)resources.GetObject("pictureBox2.Image");
+            pictureBox2.Location = new Point(271, 3);
+            pictureBox2.Name = "pictureBox2";
+            pictureBox2.Size = new Size(25, 26);
+            pictureBox2.SizeMode = PictureBoxSizeMode.Zoom;
+            pictureBox2.TabIndex = 5;
+            pictureBox2.TabStop = false;
+            // 
+            // textBox1
+            // 
+            textBox1.BorderStyle = BorderStyle.None;
+            textBox1.Location = new Point(2, 10);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(255, 16);
+            textBox1.TabIndex = 0;
+            textBox1.Text = "usernamehere";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Font = new Font("Century Gothic", 10.2F, FontStyle.Regular, GraphicsUnit.Point);
+            label3.Location = new Point(43, 322);
+            label3.Name = "label3";
+            label3.Size = new Size(93, 21);
+            label3.TabIndex = 3;
+            label3.Text = "Username";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Font = new Font("Century Gothic", 15F, FontStyle.Regular, GraphicsUnit.Point);
+            label2.Location = new Point(122, 236);
+            label2.Name = "label2";
+            label2.Size = new Size(217, 31);
+            label2.TabIndex = 2;
+            label2.Text = "Welcome back !";
             // 
             // pictureBox1
             // 
             pictureBox1.Image = (Image)resources.GetObject("pictureBox1.Image");
-            pictureBox1.Location = new Point(-8, -3);
+            pictureBox1.Location = new Point(96, 73);
             pictureBox1.Name = "pictureBox1";
-            pictureBox1.Size = new Size(903, 389);
-            pictureBox1.SizeMode = PictureBoxSizeMode.StretchImage;
-            pictureBox1.TabIndex = 5;
+            pictureBox1.Size = new Size(68, 63);
+            pictureBox1.SizeMode = PictureBoxSizeMode.Zoom;
+            pictureBox1.TabIndex = 0;
             pictureBox1.TabStop = false;
             // 
-            // grb_DN
+            // label1
             // 
-            grb_DN.BackColor = Color.White;
-            grb_DN.Controls.Add(ptr_DN);
-            grb_DN.Controls.Add(txt_TenDN);
-            grb_DN.Controls.Add(button1);
-            grb_DN.Controls.Add(txt_MK);
-            grb_DN.Font = new Font("Times New Roman", 9F, FontStyle.Bold | FontStyle.Italic, GraphicsUnit.Point);
-            grb_DN.Location = new Point(12, 12);
-            grb_DN.Name = "grb_DN";
-            grb_DN.Size = new Size(346, 344);
-            grb_DN.TabIndex = 8;
-            grb_DN.TabStop = false;
-            grb_DN.Text = "Đăng nhập";
+            label1.AutoSize = true;
+            label1.Font = new Font("Century Gothic", 19.8000011F, FontStyle.Regular, GraphicsUnit.Point);
+            label1.Location = new Point(170, 83);
+            label1.Name = "label1";
+            label1.Size = new Size(201, 40);
+            label1.TabIndex = 1;
+            label1.Text = "Shirt Haven";
             // 
-            // ptr_DN
+            // label5
             // 
-            ptr_DN.Image = (Image)resources.GetObject("ptr_DN.Image");
-            ptr_DN.Location = new Point(130, 35);
-            ptr_DN.Name = "ptr_DN";
-            ptr_DN.Size = new Size(119, 104);
-            ptr_DN.SizeMode = PictureBoxSizeMode.StretchImage;
-            ptr_DN.TabIndex = 4;
-            ptr_DN.TabStop = false;
-            // 
-            // txt_TenDN
-            // 
-            txt_TenDN.Location = new Point(53, 163);
-            txt_TenDN.Name = "txt_TenDN";
-            txt_TenDN.PlaceholderText = "Tên đăng nhập";
-            txt_TenDN.Size = new Size(272, 25);
-            txt_TenDN.TabIndex = 0;
+            label5.AutoSize = true;
+            label5.Font = new Font("Century Gothic", 10.2F, FontStyle.Regular, GraphicsUnit.Point);
+            label5.Location = new Point(96, 149);
+            label5.Name = "label5";
+            label5.Size = new Size(294, 21);
+            label5.TabIndex = 7;
+            label5.Text = "Easy and fast prodoct Manament";
             // 
             // button1
             // 
-            button1.Location = new Point(142, 265);
+            button1.BackColor = Color.FromArgb(255, 128, 255);
+            button1.Font = new Font("Century Gothic", 12F, FontStyle.Regular, GraphicsUnit.Point);
+            button1.Location = new Point(43, 515);
             button1.Name = "button1";
-            button1.Size = new Size(94, 29);
-            button1.TabIndex = 2;
-            button1.Text = "Đăng nhập";
-            button1.UseVisualStyleBackColor = true;
-            // 
-            // txt_MK
-            // 
-            txt_MK.Location = new Point(53, 214);
-            txt_MK.Name = "txt_MK";
-            txt_MK.PlaceholderText = "Mật khẩu";
-            txt_MK.Size = new Size(272, 25);
-            txt_MK.TabIndex = 1;
-            txt_MK.UseSystemPasswordChar = true;
+            button1.Size = new Size(305, 35);
+            button1.TabIndex = 8;
+            button1.Text = "Login";
+            button1.UseVisualStyleBackColor = false;
             // 
             // Main
             // 
-            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleDimensions = new SizeF(8F, 17F);
             AutoScaleMode = AutoScaleMode.Font;
-            BackColor = SystemColors.ButtonFace;
-            ClientSize = new Size(893, 386);
-            Controls.Add(grb_DN);
-            Controls.Add(pictureBox1);
-            Controls.Add(label1);
+            BackColor = Color.FromArgb(236, 240, 241);
+            ClientSize = new Size(950, 650);
+            Controls.Add(panel1);
+            Font = new Font("Century Gothic", 7.8F, FontStyle.Regular, GraphicsUnit.Point);
+            FormBorderStyle = FormBorderStyle.None;
             Name = "Main";
+            StartPosition = FormStartPosition.CenterScreen;
             Text = "Đăng nhập";
+            panel1.ResumeLayout(false);
+            panel1.PerformLayout();
+            panel3.ResumeLayout(false);
+            panel3.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)pictureBox3).EndInit();
+            panel2.ResumeLayout(false);
+            panel2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)pictureBox2).EndInit();
             ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
-            grb_DN.ResumeLayout(false);
-            grb_DN.PerformLayout();
-            ((System.ComponentModel.ISupportInitialize)ptr_DN).EndInit();
             ResumeLayout(false);
-            PerformLayout();
         }
 
         #endregion
-        private Label label1;
+
+        private Panel panel1;
         private PictureBox pictureBox1;
-        private GroupBox grb_DN;
-        private PictureBox ptr_DN;
-        private TextBox txt_TenDN;
+        private Label label1;
+        private Label label2;
+        private Panel panel2;
+        private Label label3;
+        private PictureBox pictureBox2;
+        private TextBox textBox1;
+        private Panel panel3;
+        private PictureBox pictureBox3;
+        private TextBox textBox2;
+        private Label label4;
+        private Label label5;
         private Button button1;
-        private TextBox txt_MK;
     }
 }
--- PRL/Forms/Main.cs
+++ PRL/Forms/Main.cs
@@ -17,6 +17,5 @@ namespace PRL.Forms
             InitializeComponent();
         }
 
-
     }
 }
