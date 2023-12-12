d558206a557640c8af66e09e69211f02a3e0a687
khanhlv <khanh.le1107@gmail.com>
2023-12-12 17:02:19 +0700

    Update sale lock

--- PRL/Forms/Sale.cs
+++ PRL/Forms/Sale.cs
@@ -10,7 +10,6 @@ namespace PRL.Forms
     {
         private SaleSV saleSV = new SaleSV();
 
-
         public Sale()
         {
             InitializeComponent();
@@ -25,6 +24,11 @@ namespace PRL.Forms
         {
             dtg_Sale.DataSource = null;
             dtg_Sale.Rows.Clear();//Clear dữ liệu trước khi tìm kiếm
+            foreach(DataGridViewRow row in dtg_Sale.Rows)
+            {
+                row.DefaultCellStyle.BackColor = Color.White;
+                row.DefaultCellStyle.ForeColor = Color.Black;
+            }
             dtg_Sale.ColumnCount = 8;
             dtg_Sale.Columns[0].Name = "STT";
             dtg_Sale.Columns[1].Name = "Tên Khuyến mãi";
@@ -40,9 +44,8 @@ namespace PRL.Forms
 
             for (int i = 0; i < khuyenmais.Count; i++)
             {
-
                 dtg_Sale.Rows.Add(stt++, khuyenmais[i].Tenmakhuyenmai, khuyenmais[i].Thoigianbatdau, khuyenmais[i].Thoigianketthuc, khuyenmais[i].Giamgia, khuyenmais[i].Mota, khuyenmais[i].Id, khuyenmais[i].Trangthai);
-                if (khuyenmais[i].Trangthai == "Dừng hoạt động")
+                if (khuyenmais[i].Trangthai != "Dừng hoạt động")
                 {
                     dtg_Sale.Rows[i].DefaultCellStyle.BackColor = Color.OrangeRed;
                     dtg_Sale.Rows[i].DefaultCellStyle.ForeColor = Color.Black;
@@ -52,7 +55,6 @@ namespace PRL.Forms
                     dtg_Sale.Rows[i].DefaultCellStyle.BackColor = Color.Green;
                     dtg_Sale.Rows[i].DefaultCellStyle.ForeColor = Color.White;
                 }
-
             }
         }
 
@@ -64,7 +66,6 @@ namespace PRL.Forms
 
         private void dtg_Sale_CellClick(object sender, DataGridViewCellEventArgs e)
         {
-
             txt_TenSale.Text = dtg_Sale.CurrentRow.Cells[1].Value.ToString();
             dtpk_StartDate.Value = (DateTime)dtg_Sale.CurrentRow.Cells[2].Value;
             dtpk_EndDate.Value = (DateTime)dtg_Sale.CurrentRow.Cells[3].Value;
@@ -74,19 +75,20 @@ namespace PRL.Forms
 
         private void btn_Update_Click(object sender, EventArgs e)
         {       // track ngày kết thúc lớn nhất là 1 năm tính từ ngày bắt đầu
-            if(!SaleValidate.CheckEnddatetoolong(dtpk_EndDate.Value, dtpk_StartDate.Value))
+            if (!SaleValidate.CheckEnddatetoolong(dtpk_EndDate.Value, dtpk_StartDate.Value))
             {
                 MessageBox.Show("The end date can't exceed 1 year from the start date", "Inform");
                 return;
             }
             // ngày bắt đầu mới phải bằng hoặc sau ngày bắt đầu cũ
-            if (SaleValidate.CheckifstartdateUpdate(dtpk_StartDate.Value,DateTime.Parse(dtg_Sale.CurrentRow.Cells[2].Value.ToString())))
+            if (SaleValidate.CheckifstartdateUpdate(dtpk_StartDate.Value, DateTime.Parse(dtg_Sale.CurrentRow.Cells[2].Value.ToString())))
             {
                 MessageBox.Show("New start date has to be the same or greater than the old one", "Inform");
                 return;
             }
             //  check ngày hết tồn tại trước ngày bắt đầu
-            if (SaleValidate.Checkifstartdatelaterenddate(dtpk_StartDate.Value, dtpk_EndDate.Value)){
+            if (SaleValidate.Checkifstartdatelaterenddate(dtpk_StartDate.Value, dtpk_EndDate.Value))
+            {
                 MessageBox.Show("End date can't be before start date", "Inform");
                 return;
             }
@@ -134,9 +136,9 @@ namespace PRL.Forms
                 return;
             }
             // ngày bắt đầu mới phải bằng hoặc sau ngày tạo
-            if (SaleValidate.Checkifstartdate(dtpk_StartDate.Value))
+            if (!SaleValidate.Checkifstartdate(dtpk_StartDate.Value))
             {
-                MessageBox.Show("New start date has to be the same or greater than the old one", "Inform");
+                MessageBox.Show("New discount start date has to be greater than current date", "Inform");
                 return;
             }
             //  check ngày hết tồn tại trước ngày bắt đầu
@@ -148,7 +150,7 @@ namespace PRL.Forms
             // check tên sale đã tồn tại
             if (SaleValidate.CheckIfSaleNameExist(txt_TenSale.Text))
             {
-                MessageBox.Show("Name already exist!", "Inform");
+                MessageBox.Show("Name already exist", "Inform");
                 return;
             }
             else
@@ -162,7 +164,6 @@ namespace PRL.Forms
                     Thoigianketthuc = dtpk_EndDate.Value,
                     Giamgia = int.Parse(txt_MinBill.Text),
                     Mota = txt_MoTa.Text
-
                 };
                 khuyenmai.Trangthai = ("Hoạt động");
 
@@ -187,6 +188,7 @@ namespace PRL.Forms
             List<Khuyenmai> khuyenmais = saleSV.GetKhuyenMaiByName(txt_Search.Text);
             LoadDataToGridview(khuyenmais);
         }
+
         public void clear()
         {
             txt_TenSale.Text = null;
@@ -198,10 +200,22 @@ namespace PRL.Forms
 
         private void btn_Delete_Click(object sender, EventArgs e)
         {
+            Guid currentID = (Guid)dtg_Sale.CurrentRow.Cells[6].Value;
+            var lstKM = saleSV.GetKM();
+            foreach (Khuyenmai khuyenmai in lstKM)
+            {
+                if (khuyenmai.Id == currentID)
+                {
+                    if (khuyenmai.Thoigianketthuc <= DateTime.Today)
+                    {
+                        MessageBox.Show("Can't unlock a discount that has expired");
+                        return;
+                    }
+                }
+            };
             var option = MessageBox.Show("Confirm", "Inform", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
             if (option == DialogResult.Yes)
             {
-                Guid currentID = (Guid)dtg_Sale.CurrentRow.Cells[6].Value;
                 saleSV.Delete(currentID);
                 List<Khuyenmai> khuyenmais = saleSV.GetKM();
                 LoadDataToGridview(khuyenmais);
@@ -211,7 +225,6 @@ namespace PRL.Forms
             {
                 return;
             }
-
         }
 
         private void btn_clear_Click(object sender, EventArgs e)
